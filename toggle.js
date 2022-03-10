750652828e0491a4a457069a04d6d290b80fcdc2
GrischK <grischka.gorski@laposte.net>
2022-03-11 00:25:40 +0100

    toggle sur bouton avec ajout class sur share_block

--- toggle.js
+++ toggle.js
@@ -1,4 +1,9 @@
 const button = document.querySelector(".share_button");
+const shareBlock = document.querySelector(".not-visible");
 
 console.log(button);
+console.log(shareBlock);
 
+button.addEventListener("click", () => {
+    shareBlock.classList.toggle("hidden_share_block");
+}); 
